ea188c21da91eec6bbea548da3d4b57cd8c17d7d
Mac Gyvercy <macgyvercy@gmail.com>
2020-03-04 11:24:49 +0330

    AM-4589 Remove compiler to use standard WP functions

--- functions.php
+++ functions.php
@@ -6,20 +6,26 @@ require_once( get_template_directory() . '/lib/init.php' );
 /**
  * Enqueue assets.
  *
- * Add theme style and script to Jupiter X assets files.
+ * Add custom style and script.
  */
 jupiterx_add_smart_action( 'wp_enqueue_scripts', 'jupiterx_child_enqueue_scripts', 8 );
 
 function jupiterx_child_enqueue_scripts() {
 
-	if ( function_exists( 'jupiterx_compiler_add_fragment' ) ) {
-		// Add the theme style as a fragment to have access to all the variables.
-		jupiterx_compiler_add_fragment( 'jupiterx', get_stylesheet_directory_uri() . '/assets/less/style.less', 'less' );
+	// Add custom script.
+	wp_enqueue_style(
+		'jupiterx-child',
+		get_stylesheet_directory_uri() . '/assets/css/style.css'
+	);
 
-		// Add the theme script as a fragment.
-		jupiterx_compiler_add_fragment( 'jupiterx', get_stylesheet_directory_uri() . '/assets/js/script.js', 'js' );
-	}
-
+	// Add custom script.
+	wp_enqueue_script(
+		'jupiterx-child',
+		get_stylesheet_directory_uri() . '/assets/js/script.js',
+		[ 'jquery' ],
+		false,
+		true
+	);
 }
 
 /**
